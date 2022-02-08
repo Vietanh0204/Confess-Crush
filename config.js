437d2ea89fc2948a941daae01d071ab20bf07853
Vietanh0204 <99267409+Vietanh0204@users.noreply.github.com>
2022-02-08 21:27:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tớ giao tớ cho bạn này , đấy tớ bạn đấy.',
+    messDesc: 'Tớ cho Hanh trái tim này đấy.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
